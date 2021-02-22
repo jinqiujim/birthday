e50681a541454a35b83040128d0d42489800d417
jinqiujim <79146978+jinqiujim@users.noreply.github.com>
2021-02-22 22:36:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ var config = {
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        light_candle: "蜡烛",
         wish_message: "你生日啦",
         story: "还有？",
     }
